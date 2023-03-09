8dc23c40049a7a3e49dc54d0a70844cd92264a7d
aerkha <126641782+aerkha@users.noreply.github.com>
2023-03-09 15:45:45 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -38,3 +38,30 @@ with st.sidebar:
                                   options=df['New Group'].unique(),
                                   default=df['New Group'].unique()
                                   )
+    
+total_oppty = int(df1['SF Opportunity ID'].unique().count())
+total_rev = float(df1['Revenue'].sum())
+total_rev2023 = float(df1['Rev 2023'].sum())
+total_accounts = int(df1['Account Name'].unique().count())
+
+
+total1,total2,total3,total4 = st.columns(4,gap='large')
+
+with total1:
+    st.image('<a href="https://www.flaticon.com/free-icons/opportunity" title="opportunity icons">Opportunity icons created by iconixar - Flaticon</a>',use_column_width='Auto')
+    st.metric(label = 'Total Oppty', value= numerize(total_oppty))
+    
+with total2:
+    st.image('<a href="https://www.flaticon.com/free-icons/money" title="money icons">Money icons created by turkkub - Flaticon</a>',use_column_width='Auto')
+    st.metric(label='Total Revenue (MRC+OTC)', value=numerize(total_rev))
+    
+with total3:
+    st.image('<a href="https://www.flaticon.com/free-icons/piggy-bank" title="piggy bank icons">Piggy bank icons created by Freepik - Flaticon</a>',use_column_width='Auto')
+    st.metric(label= 'Total Est. Rev 2023',value=numerize(total_rev2023))
+
+with total4:
+    st.image('<a href="https://www.flaticon.com/free-icons/work" title="work icons">Work icons created by geotatah - Flaticon</a>',use_column_width='Auto')
+    st.metric(label='Total Customers',value=numerize(total_accounts))
+    
+
+
