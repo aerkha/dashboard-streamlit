22176f087d67cab0dd0e8025ea2b122f69ef4d0a
aerkha <126641782+aerkha@users.noreply.github.com>
2023-03-21 08:25:36 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-#import plotly_express as px
+import plotly_express as px
 from numerize.numerize import numerize
 
 st.set_page_config(
@@ -9,10 +9,12 @@ st.set_page_config(
     initial_sidebar_state='collapsed'
 )
 
+
 @st.cache_data
 def get_data():
-    df = pd.read_csv('https://github.com/aerkha/isatb2b/blob/main/Marketing_Database.csv')
+    df = pd.read_csv('Marketing_Database.csv')
     return df
+
 
 df = get_data()
 
@@ -35,35 +37,70 @@ with st.sidebar:
                                   default=df['Stage'].unique()
                                   )
     Group_filter = st.multiselect(label='Select Group',
-                                  options=df['New Group'].unique(),
-                                  default=df['New Group'].unique()
+                                  options=df['New_Group'].unique(),
+                                  default=df['New_Group'].unique()
                                   )
 
-df1 = df.query('campaign == @Campaign_filter & stage == @Stage_filter & group == @Group_filter')
+df1 = df.query('Campaign == @Campaign_filter and Stage == @Stage_filter and New_Group == @Group_filter')
 
-total_oppty = int(df1['SF Opportunity ID'].unique().count())
-total_rev = float(df1['Revenue'].sum())
-total_rev2023 = float(df1['Rev 2023'].sum())
-total_accounts = int(df1['Account Name'].unique().count())
+df_oppty = pd.DataFrame(df1['SF_Opportunity_Id'].value_counts())
+df_ca = pd.DataFrame(df1['Account_Name'].value_counts())
 
+total_oppty = int(df_oppty.count())
+total_rev = float(df1['Rev'].sum())
+total_rev2023 = float(df1['Rev_2023'].sum())
+total_accounts = int(df_ca.count())
 
-total1,total2,total3,total4 = st.columns(4,gap='large')
+total1, total2, total3, total4 = st.columns(4, gap='large')
 
 with total1:
-    st.image('<a href="https://www.flaticon.com/free-icons/opportunity" title="opportunity icons">Opportunity icons created by iconixar - Flaticon</a>',use_column_width='Auto')
-    st.metric(label = 'Total Oppty', value= numerize(total_oppty))
-    
+    st.image('Image/opportunity.png',use_column_width='auto')
+    st.metric(label='Total Oppty', value=numerize(total_oppty))
+
 with total2:
-    st.image('<a href="https://www.flaticon.com/free-icons/money" title="money icons">Money icons created by turkkub - Flaticon</a>',use_column_width='Auto')
+    st.image('Image/revenue.png',use_column_width='auto')
     st.metric(label='Total Revenue (MRC+OTC)', value=numerize(total_rev))
-    
+
 with total3:
-    st.image('<a href="https://www.flaticon.com/free-icons/piggy-bank" title="piggy bank icons">Piggy bank icons created by Freepik - Flaticon</a>',use_column_width='Auto')
-    st.metric(label= 'Total Est. Rev 2023',value=numerize(total_rev2023))
+    st.image('Image/rev2023.png',use_column_width='auto')
+    st.metric(label='Total Est. Rev 2023', value=numerize(total_rev2023))
 
 with total4:
-    st.image('<a href="https://www.flaticon.com/free-icons/work" title="work icons">Work icons created by geotatah - Flaticon</a>',use_column_width='Auto')
-    st.metric(label='Total Customers',value=numerize(total_accounts))
-    
+    st.image('Image/account.png',use_column_width='auto')
+    st.metric(label='Total Customers', value=numerize(total_accounts))
 
+Q1,Q2 = st.columns(2)
 
+with Q1:
+    df2 = df1.groupby(by = ['Campaign']).sum()['Rev'].reset_index()
+    df2['Rev'] =round(df2['Rev'] ,2)
+    fig_rev_by_campaign = px.bar(df2,
+                            x='Campaign',
+                            y='Rev',
+                            title='<b>Revenue (OTC MRC)</b>')
+    fig_rev_by_campaign.update_layout(title = {'x' : 0.5},
+                                    plot_bgcolor = "rgba(0,0,0,0)",
+                                    xaxis =(dict(showgrid = False)),
+                                    yaxis =(dict(showgrid = False)))
+    st.plotly_chart(fig_rev_by_campaign,use_container_width=True)
+
+with Q2:
+    df3 = df1.groupby(by=['Campaign']).sum()['Rev_2023'].reset_index()
+    df3['Rev_2023'] = round(df3['Rev_2023'], 2)
+    fig_rev23_by_campaign = px.bar(df3,
+                                 x='Campaign',
+                                 y='Rev_2023',
+                                 title='<b>Est. Revenue 2023</b>')
+    fig_rev23_by_campaign.update_layout(title={'x': 0.5},
+                                      plot_bgcolor="rgba(0,0,0,0)",
+                                      xaxis=(dict(showgrid=False)),
+                                      yaxis=(dict(showgrid=False)))
+    st.plotly_chart(fig_rev23_by_campaign, use_container_width=True)
+
+Q3,Q4 = st.columns(2)
+
+with Q3:
+    df4 = df_oppty.groupby(by='SF_Opprtunity_Id').sum()[['Stage']].reset_index()
+    fig_spend_by_gender = px.pie(df4,names='stage',values='SF_Opprtunity_Id',title='<b>Stage</b>')
+    fig_spend_by_gender.update_layout(title = {'x':0.5}, plot_bgcolor = "rgba(0,0,0,0)")
+    st.plotly_chart(fig_spend_by_gender,use_container_width=True)
